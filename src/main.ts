06c9066be72d35ed1fd47201070e2237fe4aaab2
owen-prest# <dev.owenpresthotmail.com>
2026-06-18 21:02:27 +0100

    Set up Vue app and router

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
