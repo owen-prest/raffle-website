85b01310c949bd5ff828f440d87a912ea51165e4
owen-prest# <dev.owenpresthotmail.com>
2026-06-19 22:25:38 +0100

    Added vue-router, loginAuth, and vue imports

--- src/Views/Profile.vue
+++ src/Views/Profile.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-  import profileImg from '@/assets/images/profileImg.webp'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { logout } from '@/stores/auth'
+import profileImg from '@/assets/images/profileImg.webp'
 </script>
 
 <template>
@@ -10,9 +13,9 @@
         <img class="profile-image" :src="profileImg" alt="profile image">
       </div>
       <div class="profile-info">
-        <h1>Your Username</h1><span>your username</span>
-        <h1>Change Password</h1><span>dont show password</span>
-        <h1>Your Bio</h1>
+        <h1 class="profile-text">Your Username</h1><span class="profile-text">your username</span>
+        <h1 class="profile-text">Change Password</h1><span class="profile-text">dont show password</span>
+        <h1 class="profile-text">Your Bio</h1>
       </div>
     </div>
   </div>
@@ -22,11 +25,39 @@
 .profile{
     background-color: red;
     display: flex;
-}
-  .profile-menu{
-    display: inline-block;
-    justify-content: center;
     width: 100%;
-    background-color: aqua;
-  }
+    background-color: #16263A;
+
+}
+.profile-menu{
+  display: inline-block;
+  justify-content: center;
+  width: 100%;
+  background-color: transparent;
+  padding: 40px;
+}
+.image-wrapper{
+  display: flex;
+  background-color: transparent;
+}
+.profile-image{
+  max-height:150px;
+  border-radius: 50%;
+  border: 2px solid grey;
+}
+.profile-menu{
+  background-color: transparent;
+  width: 100%;
+}
+.profile-info{
+  background-color: transparent;
+  border: 1px solid grey;
+  border-radius: 12px;
+}
+.profile-text{
+  background-color: transparent;
+  color: #e6edef;
+  padding: 10px;
+  font-size: 22px;
+}
 </style>
