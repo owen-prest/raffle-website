56a77ff5d5ab129b0d2ccff37f1ea3033d32b087
owen-prest# <dev.owenpresthotmail.com>
2026-06-19 03:32:25 +0100

    Created a banner and gallery section foundations

--- src/Views/Dashboard.vue
+++ src/Views/Dashboard.vue
@@ -1,9 +1,23 @@
-<script>
-
+<script setup lang="ts">
+  import Gallery from '@/components/Gallery.vue'
+  import Banner from '@/components/Banner.vue'
+  import { RouterLink } from 'vue-router';
 </script>
 
 <template>
   <div class="home">
-    <h1>Welcome to the Home Page</h1>
+    <Banner/>
+     <Gallery/>
   </div>
 </template>
+
+<style>
+.home-title{
+  color: #E6EDF3;
+  font-size: 32px;
+  padding: 35px 0 35px 40px;
+}
+.home{
+  background-color: #0B1220;
+}
+</style>
