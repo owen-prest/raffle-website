e09cb8e143f22a9747d710a5821e270393ec9e66
owen-prest# <dev.owenpresthotmail.com>
2026-06-19 03:34:03 +0100

    Changed the banner and gallery to be components that are imported

---
+++ src/components/Banner.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+  import bannerimage from '@/assets/images/banner.jpg'
+</script>
+
+<template>
+ <h1 class="home-title">Welcome to Winna Raffle!</h1>
+  <div class="banner">
+    <div class="banner-overlay">
+      <div class="banner-text">You could be a winner today!</div>
+      <div class="banner-text" to="/Login">
+        <RouterLink class="link-hover shake" to="/Login">Sign up today!</RouterLink>
+      </div>
+    </div>
+    <img class="banner-image" :src="bannerimage" alt="raffle image">
+  </div>
+</template>
+
+<style>
+  .banner{
+    position: relative;
+    width: 100%;
+  }
+  .banner-image{
+    width: 100%;
+    max-height: 500px;
+    object-fit:cover;
+    display: block; /* removes the small gap below the image */
+  }
+  .banner-overlay{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column; /* stacks text on top of each other */
+    align-items: center;
+    gap: 10px;
+    background-color: rgba(22, 38, 58, 0.5);
+    padding: 20px;
+    border-radius: 10px;
+  }
+  .banner-text{
+    color:#E6EDF3;
+    text-align: center;
+    font-size: 45px;
+    background-color: transparent;
+  }
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import placeholder from '@/assets/images/placeholder.webp'
+</script>
+
+<template>
+  <div class="gallery">
+    <h1 class="home-title">Gallery</h1>
+    <div class="gallery-grid">
+      <img class="gallery-image" :src="placeholder" alt="placeholder img">
+      <img class="gallery-image" :src="placeholder" alt="placeholder img">
+      <img class="gallery-image" :src="placeholder" alt="placeholder img">
+    </div>
+  </div>
+</template>
+
+<style>
+  .gallery{
+    width: 100%;
+    padding: 0px;
+  }
+  .gallery-grid{
+    display: flex;
+    padding: 35px 0;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    background-color: #16263A;
+  }
+  .gallery-image{
+    max-height: 300px;
+    margin: 0px 30px;
+    border-radius: 10%;
+  }
+</style>
